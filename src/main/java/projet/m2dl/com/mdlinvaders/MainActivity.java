8aae1755ee0045efe24a3934fd18b927a6c98455
fabien <fabien.rolfo@master-developpement-logiciel.fr>
2015-01-22 10:04:56 +0100

    toto

--- src/main/java/projet/m2dl/com/mdlinvaders/MainActivity.java
+++ src/main/java/projet/m2dl/com/mdlinvaders/MainActivity.java
@@ -1,9 +1,11 @@
 package projet.m2dl.com.mdlinvaders;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchGame(View view){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
